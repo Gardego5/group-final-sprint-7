e07832b8bb0900044c5f8f89218b16b6510ac91d
David Preciado <almostvenice@gmail.com>
2022-08-29 10:42:55 -0700

    rgb

--- client/src/components/ModalComponents/BasicButton.jsx
+++ client/src/components/ModalComponents/BasicButton.jsx
@@ -4,7 +4,7 @@ import { styled } from 'styled-components';
 const StyledButton = styled.button`
     width: ${({ w }) => w ? w : "80px"};
     height: ${({ h }) => h ? h : "30px"};
-    background: ${({ bg }) => bg ? bg : "black"};
+    background: ${({ bg }) => bg ? bg : "rgb(27, 160, 152)"};
 `
 
 const BasicButton = () => {
