aafea092d4702de09d04e94ed8cc6514b9fce8b1
Ryan Villarreal <ryan.villarreal24@yahoo.com>
2022-08-29 11:14:22 -0500

    Implemented routing

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,19 @@
+import { Fragment } from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+
+import Company from "./pages/Company";
 import Login from "./pages/Login";
 
 function App() {
   return (
-    <div className="App">
-      <Login/>
-    </div>
+    <Fragment>
+      <Router>
+        <Switch>
+          <Route path="/company" render={() => <Company />} />
+          <Route path="/" render={() => <Login />} />
+        </Switch>
+      </Router>
+    </Fragment>
   );
 }
 
