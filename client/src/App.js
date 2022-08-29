8344fa10a7e54b7da7cfda7668e24f0ade7ed71a
Tamblin Papendorp <tamblinpap@gmail.com>
2022-08-29 12:23:49 -0400

    basic SelectCompany page conponents added

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,15 @@
 import { Fragment } from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
-import Company from "./pages/Company";
 import Login from "./pages/Login";
+import SelectCompany from "./pages/SelectCompany";
 
 function App() {
   return (
     <Fragment>
       <Router>
         <Switch>
-          <Route path="/company" render={() => <Company />} />
+          <Route path="/company" render={() => <SelectCompany />} />
           <Route path="/" render={() => <Login />} />
         </Switch>
       </Router>
