9315ce9f5d9352e7c901d51d97e0d93eb4e1d2f8
Gardego5 <garrett.davis.7777@gmail.com>
2022-08-29 12:51:19 -0700

    Fix Routing

--- client/src/App.js
+++ client/src/App.js
@@ -12,11 +12,11 @@ function App() {
     <Fragment>
       <Router>
         <Switch>
-          <Route path="/announcements" render={() => <Announcements />} />
-          <Route path="/projects" render={() => <Projects />} />
-          <Route path="/company" render={() => <SelectCompany />} />
-          <Route path="/teams" render={() => <Teams />} />
-          <Route path="/" render={() => <Login />} />
+          <Route path="/announcements" component={Announcements} />
+          <Route path="/projects" component={Projects} />
+          <Route path="/company" component={SelectCompany} />
+          <Route path="/teams" component={Teams} />
+          <Route path="/" component={Login} />
         </Switch>
       </Router>
     </Fragment>
