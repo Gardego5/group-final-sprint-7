215366c137ca78906219c4fdab3cdeebdc777acf
Brandon Hernandez <brandoncinthia0621@gmail.com>
2022-08-29 17:49:20 -0600

    created team service impl

---
+++ Sprint7Final/src/main/java/com/example/Sprint7Final/service/impl/TeamServiceImpl.java
@@ -0,0 +1,22 @@
+package com.example.Sprint7Final.service.impl;
+
+import java.util.List;
+
+import com.example.Sprint7Final.dtos.TeamDto;
+import com.example.Sprint7Final.services.TeamService;
+
+public class TeamServiceImpl implements TeamService {
+
+	@Override
+	public List<TeamDto> getTeams() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public TeamDto getTeamById(Long teamId) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
