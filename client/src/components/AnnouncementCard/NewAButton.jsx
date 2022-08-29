049afc39159063238d08c103ee2fe74c6942ce23
Jesus Milan <milan.jesusdev@gmail.com>
2022-08-29 19:30:32 -0400

    added navbar fixed new button

---
+++ client/src/components/AnnouncementCard/NewAButton.jsx
@@ -0,0 +1,7 @@
+import BasicButton from '../ModalComponents/BasicButton';
+
+const NewAButton = () => {
+  return <BasicButton className="new-button">New</BasicButton>;
+};
+
+export default NewAButton;
--- client/src/components/Modals/CreateAnnouncement.jsx
+++ client/src/components/Modals/CreateAnnouncement.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import { useDispatch } from "react-redux";
-import 'bootstrap/dist/css/bootstrap.min.css';
+import "bootstrap/dist/css/bootstrap.min.css";
 import {
   Button,
   Modal,
@@ -11,30 +11,32 @@ import {
 } from "reactstrap";
 import { Formik, Field, Form } from "formik";
 
-const CreateAnnouncement = ({posterName}) => {
+const CreateAnnouncement = ({ posterName }) => {
   const [modalOpen, setModalOpen] = useState(false);
 
-  const dispatch = useDispatch(); 
+  const dispatch = useDispatch();
   const toggle = () => setModalOpen(!modalOpen);
   const handleSubmit = (values) => {
     const announcement = {
       posterName: posterName,
-      postText: values.postText, 
+      postText: values.postText,
       date: new Date(Date.now()).toISOString(), //create a new [Date] object and set it to the time the form was submitted
     };
 
-    // dispatch(postAnnouncement(announcement)); // dispatch the action to update the state of the component 
+    // dispatch(postAnnouncement(announcement)); // dispatch the action to update the state of the component
     // setModalOpen(false); //when submitted the modal closes as the [modalOpen] is set to [false] by the [useState]/[setModalOpen]
   };
   return (
     <>
-      <Button outline onClick={() => setModalOpen(true)}>       
+      <Button outline onClick={() => setModalOpen(true)}>
         Add Announcement
       </Button>
       <Modal isOpen={modalOpen} toggle={toggle}>
         <ModalHeader>
           Add Announcement
-          <Button color="danger" onClick={() => setModalOpen(false)}>X</Button>
+          <Button color="danger" onClick={() => setModalOpen(false)}>
+            X
+          </Button>
         </ModalHeader>
         <ModalBody>
           <Formik
