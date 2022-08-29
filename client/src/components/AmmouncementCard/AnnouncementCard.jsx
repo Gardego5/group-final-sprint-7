72e76864a91597ab97e1d6cc9c5d8857b28c8035
Jesus Milan <milan.jesusdev@gmail.com>
2022-08-29 11:53:29 -0400

    added announcement files

---
+++ client/src/components/AmmouncementCard/AnnouncementCard.jsx
@@ -0,0 +1,15 @@
+const AnnouncementCard = () => {
+    return ( <div>
+        <div className="card">
+            <div className="card-header">
+                <h5 className="user">Chris, CEO</h5>
+                <span className="date-posted"></span>
+            </div>
+            <div className="card-body">
+                <p className="announcement">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat natus dolore nemo, quo voluptates saepe, tempore in porro sed officia nostrum autem. Dolorem itaque pariatur autem in, ipsum excepturi harum?</p>
+            </div>
+        </div>
+    </div> );
+}
+ 
+export default AnnouncementCard;
---
+++ client/src/components/AmmouncementCard/NewAButton.jsx
@@ -0,0 +1,5 @@
+const NewAButton = () => {
+    return ( <button className="new-button">New</button> );
+}
+ 
+export default NewAButton;
