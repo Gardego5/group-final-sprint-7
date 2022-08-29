549494d9fb599728bab04bcdadc00eb074804939
David Preciado <almostvenice@gmail.com>
2022-08-29 10:41:22 -0700

    brace

--- client/src/components/ModalComponents/CloseButton.jsx
+++ client/src/components/ModalComponents/CloseButton.jsx
@@ -1,15 +1,11 @@
-import { styled } from 'styled-components';
+import { styled } from "styled-components";
 
 const StyledClose = styled.button`
-    width: ${({ w }) => w ? w : "50px"};
-    height: ${({ h }) => h ? h : "50px"};
-`
+  width: ${({ w }) => (w ? w : "50px")};
+  height: ${({ h }) => (h ? h : "50px")};
+`;
 
 const CloseButton = () => {
-    return ( 
-        <StyledClose>
-            X
-        </StyledClose>
-     ); 
- 
+  return <StyledClose>X</StyledClose>;
+};
 export default CloseButton;
