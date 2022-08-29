40a739fb5589eb3bb6117f2bd84897ff152f4472
Ryan Villarreal <ryan.villarreal24@yahoo.com>
2022-08-29 11:14:35 -0500

    Created Company page

---
+++ client/src/pages/Company.jsx
@@ -0,0 +1,22 @@
+import { Fragment } from "react";
+import styled from "styled-components";
+
+const StyledCompany = styled.div`
+  align-items: center;
+  display: flex;
+  flex-direction: column;
+`;
+
+// const StyledTitle = styled.h2`
+//   font-size: 24px; ;
+// `;
+
+const Company = () => {
+  return (
+    <StyledCompany>
+      <h1>Select Company</h1>
+    </StyledCompany>
+  );
+};
+
+export default Company;
