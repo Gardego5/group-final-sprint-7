302029afc90367da7c4f69d692f405c60ff9f0aa
NessXXIII <jesselopez1162@gmail.com>
2022-08-29 10:33:31 -0500

    properly get SpringBoot working

--- Sprint7Final/src/test/java/com/example/Sprint7Final/Sprint7FinalApplicationTests.java
+++ Sprint7Final/src/test/java/com/example/Sprint7Final/Sprint7FinalApplicationTests.java
@@ -1,10 +1,10 @@
-package com.example.demo;
+package com.example.Sprint7Final;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class DemoApplicationTests {
+class Sprint7FinalApplicationTests {
 
 	@Test
 	void contextLoads() {
