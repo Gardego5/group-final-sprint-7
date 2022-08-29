b341466b5cbbbbd74691559655e63dea5ab790c1
Tamblin Papendorp <tamblinpap@gmail.com>
2022-08-29 12:13:34 -0400

    Selected company page created

---
+++ client/src/pages/SelectCompany.jsx
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+
+const SelectCompany = () => {
+
+    return(
+        <div>
+            <h1>Select Company</h1>
+        </div>
+    );
+};
+
+export default SelectCompany;
