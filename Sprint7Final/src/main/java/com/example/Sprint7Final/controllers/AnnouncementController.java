1ec14f1124da352cde47c7f86baeb315940e49cd
Roy Duran <109109463+roy-duran@users.noreply.github.com>
2022-08-30 13:39:58 -0600

    Announcement files
    - Fixed typo
    - Added endpoint for get all announcements for a company by id
    - Needs implementation

--- Sprint7Final/src/main/java/com/example/Sprint7Final/controllers/AnnouncementController.java
+++ Sprint7Final/src/main/java/com/example/Sprint7Final/controllers/AnnouncementController.java
@@ -3,6 +3,7 @@ package com.example.Sprint7Final.controllers;
 import java.util.List;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,7 +14,7 @@ import lombok.RequiredArgsConstructor;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/company/annoucements")
+@RequestMapping("/announcements")
 public class AnnouncementController {
 	
 	private final AnnouncementService announcementService;
@@ -21,6 +22,11 @@ public class AnnouncementController {
 	@GetMapping
 	public List<AnnouncementResponseDto> getAllAnnouncements() {
 		return announcementService.getAllAnnouncements();
+	}
+	
+	@GetMapping("/{companyId}")
+	public List<AnnouncementResponseDto> getAllCompanyAnnouncements(@PathVariable Long companyId)  {
+		return announcementService.getAllCompanyAnnouncements(companyId);
 	}
 
 }
--- Sprint7Final/src/main/java/com/example/Sprint7Final/service/impl/AnnouncementServiceImpl.java
+++ Sprint7Final/src/main/java/com/example/Sprint7Final/service/impl/AnnouncementServiceImpl.java
@@ -2,14 +2,29 @@ package com.example.Sprint7Final.service.impl;
 
 import java.util.List;
 
+import org.springframework.stereotype.Service;
+
 import com.example.Sprint7Final.dtos.AnnouncementResponseDto;
+import com.example.Sprint7Final.mappers.AnnouncementMapper;
+import com.example.Sprint7Final.repositories.AnnouncementRepository;
 import com.example.Sprint7Final.services.AnnouncementService;
 
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
 public class AnnouncementServiceImpl implements AnnouncementService{
+
+	private final AnnouncementRepository announcementRepository;
+	private final AnnouncementMapper announcementMapper;
 
 	@Override
 	public List<AnnouncementResponseDto> getAllAnnouncements() {
-		// TODO Auto-generated method stub
+		return announcementMapper.entitiesToDtos(announcementRepository.findAll());
+	}
+
+	@Override
+	public List<AnnouncementResponseDto> getAllCompanyAnnouncements(Long companyId) {
 		return null;
 	}
 
