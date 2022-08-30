818c8bf90228334af039a95af95c10f52ff113fb
NessXXIII <jesselopez1162@gmail.com>
2022-08-30 13:01:25 -0500

    Added @CrossOrigin, UserServiceImpl add new exception handling

--- Sprint7Final/src/main/java/com/example/Sprint7Final/controllers/ProjectController.java
+++ Sprint7Final/src/main/java/com/example/Sprint7Final/controllers/ProjectController.java
@@ -3,10 +3,7 @@ package com.example.Sprint7Final.controllers;
 import java.util.List;
 
 import com.example.Sprint7Final.services.ProjectService;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import com.example.Sprint7Final.dtos.ProjectDto;
 
@@ -15,6 +12,7 @@ import lombok.RequiredArgsConstructor;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/project")
+@CrossOrigin
 public class ProjectController {
 	private final ProjectService projectService;
 	
--- Sprint7Final/src/main/java/com/example/Sprint7Final/service/impl/UserServiceImpl.java
+++ Sprint7Final/src/main/java/com/example/Sprint7Final/service/impl/UserServiceImpl.java
@@ -2,6 +2,7 @@ package com.example.Sprint7Final.service.impl;
 
 
 import com.example.Sprint7Final.dtos.CredentialsDto;
+import com.example.Sprint7Final.dtos.UserRequestDto;
 import com.example.Sprint7Final.entities.Credentials;
 import com.example.Sprint7Final.exceptions.BadRequestException;
 import com.example.Sprint7Final.exceptions.NotAuthorizedException;
@@ -18,6 +19,8 @@ import com.example.Sprint7Final.services.UserService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
+import java.util.Optional;
+
 @Service
 @RequiredArgsConstructor
 @Slf4j
@@ -27,19 +30,48 @@ public class UserServiceImpl implements UserService {
     private final UserMapper userMapper;
 	private final CredentialsMapper credentialsMapper;
 
-	@Override
-	public UserResponseDto getUser(CredentialsDto credentialsDto) {
-
-		log.warn(credentialsDto.getPassword() + " " + credentialsDto.getUsername());
-		if (credentialsDto.getPassword() == null || credentialsDto.getUsername() == null) {
-			throw new BadRequestException("Bad request");
-		}
-		for (User user : userRepository.findAllByDeletedFalse()) {
-			if (user.getCredentials().equals(credentialsMapper.dtoToEntity(credentialsDto))){
-				return userMapper.entityToDto(userRepository.findByCredentialsAndDeletedFalse(credentialsMapper.dtoToEntity(credentialsDto)).get());
-			}
+	public User validateUserCredentialsMatchDatabase(CredentialsDto credentialsDto) {
+		if (getUserByCredentials(credentialsDto).getCredentials().equals(credentialsMapper.dtoToEntity(credentialsDto))) {
+			return getUserByCredentials(credentialsDto);
 		}
 		throw new NotAuthorizedException("Please check your username or password");
+	}
+
+	public boolean validateCredentialsForm(CredentialsDto credentialsDto) {
+		return credentialsDto.getPassword() != null && credentialsDto.getUsername() != null;
+	}
+
+	public User getUserByCredentials(CredentialsDto credentialsDto) {
+		return getUserByUsernameReturnUserEntity(credentialsDto.getUsername());
+	}
+
+	public User getUserByUsernameReturnUserEntity(String username) {
+		Optional<User> optionalUser = userRepository.findByCredentialsUsername(username);
+		if (optionalUser.isEmpty() || optionalUser.get().isDeleted()) {
+			throw new NotFoundException("User not found with username: " + username);
+		}
+		return optionalUser.get();
+	}
+
+
+	@Override
+	public UserResponseDto getUser(CredentialsDto credentialsDto) {
+		if (validateCredentialsForm(credentialsDto)) {
+			return userMapper.entityToDto(validateUserCredentialsMatchDatabase(credentialsDto));
+		}
+		throw new BadRequestException("Bad request");
+	}
+
+	@Override
+	public UserResponseDto createUser(CredentialsDto credentialsDto, UserRequestDto userRequestDto) {
+		User user = getUserByCredentials(credentialsDto);
+		User user2 = validateUserCredentialsMatchDatabase(credentialsDto);
+		if (user2.isAdmin() && user.isActive()) {
+
+		}
+		User userToBeCreated = userMapper.dtoToEntity(userRequestDto);
+
+		return null;
 	}
 
 	@Override
