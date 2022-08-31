aeebd172cefa7c49767b7484276e3a28b794088d
NessXXIII <jesselopez1162@gmail.com>
2022-08-30 21:04:49 -0500

    Crossorigin

--- Sprint7Final/src/main/java/com/example/Sprint7Final/controllers/AnnouncementController.java
+++ Sprint7Final/src/main/java/com/example/Sprint7Final/controllers/AnnouncementController.java
@@ -14,6 +14,7 @@ import lombok.RequiredArgsConstructor;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/announcements")
+@CrossOrigin
 public class AnnouncementController {
 	
 	private final AnnouncementService announcementService;
