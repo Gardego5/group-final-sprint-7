650da09209693f73c96367adefc73819f9f8cff3
Brandon Hernandez <brandoncinthia0621@gmail.com>
2022-08-30 23:22:10 -0600

    getprojectsbycompanyid completed

--- Sprint7Final/src/main/java/com/example/Sprint7Final/service/impl/ProjectServiceImpl.java
+++ Sprint7Final/src/main/java/com/example/Sprint7Final/service/impl/ProjectServiceImpl.java
@@ -3,13 +3,14 @@ package com.example.Sprint7Final.service.impl;
 import com.example.Sprint7Final.dtos.ProjectDto;
 import com.example.Sprint7Final.entities.Project;
 import com.example.Sprint7Final.mappers.ProjectMapper;
-import com.example.Sprint7Final.mappers.TeamMapper;
+import com.example.Sprint7Final.repositories.CompanyRepository;
 import com.example.Sprint7Final.repositories.ProjectRepository;
 import com.example.Sprint7Final.repositories.TeamRepository;
 import com.example.Sprint7Final.services.ProjectService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
@@ -19,6 +20,7 @@ public class ProjectServiceImpl implements ProjectService {
 	private final ProjectRepository projectRepository;
 	private final ProjectMapper projectMapper;
 	private final TeamRepository teamRepository;
+	private final CompanyRepository companyRepository;
 
 	@Override
 	public List<ProjectDto> getAllProjects() {
@@ -46,5 +48,19 @@ public class ProjectServiceImpl implements ProjectService {
 	@Override
 	public List<ProjectDto> getProjectsByTeamId(Long teamId) {
 		return projectMapper.entitiesToDtos(teamRepository.getReferenceById(teamId).getTeamProjects());
+	}
+
+	@Override
+	public List<ProjectDto> getProjectsByCompanyId(Long companyId) {
+		
+		List<Project> projects = projectRepository.findAll();
+		List<Project> projectsToReturn = new ArrayList<>();
+		for(Project project : projects) {
+			if(project.getTeamOnProject().getTeamCompany().getId().equals(companyId)) {
+				projectsToReturn.add(project);
+			}
+			
+		}
+		return projectMapper.entitiesToDtos(projectsToReturn);
 	}
 }
